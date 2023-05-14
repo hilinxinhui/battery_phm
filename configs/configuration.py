26a9a2fbb3c1ecd61dfbb76714841cf66c8b743d
linxinhui <hilinxinhui@gmail.com>
2023-05-15 02:05:33 +0800

    Commit configuration code and delete configs/.gitkeep.

---
+++ configs/configuration.py
@@ -0,0 +1,33 @@
+import os.path as osp
+
+# 一些常量
+
+MODEL_CONFIG = {
+
+}
+
+TRAIN_CONFIG = {
+    # checkpoints保存在哪里
+
+    # 随机数种子
+
+    # 训练数据集配置
+
+    # 优化器
+
+    # 学习率
+
+    # 日志保存在哪里
+
+    # ...
+
+}
+
+TEST_CONFIG = {
+    # 日志保存在哪里
+
+    # 从哪里读取模型
+
+    # ...
+
+}
