d53517221444c338a4851b5256589a8f4c8927d9
linxinhui <hilinxinhui@gmail.com>
2023-05-17 17:05:16 +0800

    Update data preprocess code.

--- utils/visualization.py
+++ utils/visualization.py
@@ -8,24 +8,10 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.rcParams["font.family"] = ["SimHei"]  # 设置字体家族
-plt.rcParams["font.sans-serif"] = ["SimHei"]  # 设置无衬线字体
+# plt.rcParams["font.family"] = ["SimHei"]  # 设置字体家族
+# plt.rcParams["font.sans-serif"] = ["SimHei"]  # 设置无衬线字体
 
-# from torchinfo import summary
-
-
-# def print_model(model):
-#     # 直接打印torch模型
-#     print(model)
-#     # 使用torchinfo打印模型参数
-#     summary(model, (1, 3, 32, 32)) # 1：batch_size， 3：channel，输入图片size：32 * 32
-    
-
-# # tensorboard可以记录模型每一层的feature map、权重、loss等信息，利用tensorboard实现训练过程的可视化
-# from tensorboard import SummaryWriter
-# log_path = "./runs"
-# writer = SummaryWriter(log_path)
-# writer.add_graph(model, torch.rand(1, 3, 32, 32))
+plt.rcParams['font.serif'] = ['STSong']
 
 def plot_single(data, names, title):
     plt.figure(figsize=(5, 4))
